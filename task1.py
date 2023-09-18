fbaca57999d20b518e3da92fed60c4e87a7694be
kotov <kotovadasha08@gmail.com>
2023-09-18 15:12:07 +0300

    task1 by Python

---
+++ task1.py
@@ -0,0 +1,13 @@
+arr = [1, 6, 3, 5, 2, 10]
+res = [10, 14, 10, 17]
+
+def SummuryOfN(arr, m):
+    vec = []
+    tmp = 0
+    for i in range(m):
+        tmp += arr[i]
+    vec.append(tmp)
+
+    for i in range(m, len(arr)):
+        tmp = tmp + arr[i] - arr[i % m]
+        vec.append(tmp)
